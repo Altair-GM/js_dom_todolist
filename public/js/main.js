ad1d0701696ef83d71be8a8c89d6bf3a18916249
Mohamed <ghannou.0702@gmail.com>
2021-11-12 19:52:15 +0100

    c'est la merdi :'(

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,23 @@
 // Classes bbutton
 let allCmnd = document.querySelector(".allCmnd").children
-let todoTab = document.querySelector(".todoTab").children
+let todo = document.querySelector(".todo").children
+let todoTab = document.querySelector(".todoTab")
+console.log(todo);
 let input = document.querySelector("input")
+
 console.log(input);
 // button all
 let ended = allCmnd[0]
 let toDo = allCmnd[1]
 let all = allCmnd[2]
 //  button input
-let clear = todoTab[1]
-let modif = todoTab[2]
-let delet = todoTab[3]
-let add = todoTab[4]
-console.log(clear);
+let clear = todo[1]
+let modif = todo[2]
+let delet = todo[3]
+let add = todo[4]
+let clonage = [input,clear,modif,delet,add]
+console.log(clonage);
+// console.log(clear);
 // [[[[[[[[[[[[[[[[[[[Commandes generales]]]]]]]]]]]]]]]]]]]
 
 ended.addEventListener("click", () => {
@@ -38,20 +43,91 @@ clear.addEventListener("click", () => {
     }
 })
 modif.addEventListener("click", () => {
-  
-  if (input.disabled == true) {
-    input.disabled = false
-      
-  }else{
-      input.disabled = true
-  }
+    if (input.disabled == true) {
+        input.disabled = false
+    } else {
+        input.disabled = true
+    }
 })
 delet.addEventListener("click", () => {
-    input.value = "" 
-
+    input.value = ""
 })
-
+ 
+let mutiClonage = (el) =>{
+    let clone = el
+    let suite = el.cloneNode(true)
+    suite.children[0].value = ""
+    clone.appendChild(suite)
+}
 add.addEventListener("click", () => {
+mutiClonage(todo)
+console.log(conage);
+// let genliste = document.createElement("div")
+// genliste.classList.add('liste');
+// genliste.textContent = "liste"
+// console.log(genliste);
+// todoTab.appendChild(genliste)
 
-    
 })
+
+console.log();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[Experiances ratée]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+
+// let btntoDo = e.currentTarget
+// let Todo = btntoDo.parentNode
+// console.log(Todo);
+// let clone = Todo.cloneNode(true)
+
+// console.log(clone);
+
+// let i = 0 
+// for (i = 1; i < 20; i++) {
+//     let clone = todo.cloneNode(true)
+//     clone.class = "todo"+i
+//     // document.getElementsByTagName("div").appendChild(clone)   
+// }
+// clonage.forEach(el => {
+//     el.cloneNode(true)   
+//     // el.name = el + "_" + i 
+//     // let espere = document.getElementsByClassName(".todo").appendChild(el)      
+//     i++
+// });
+// i++
+
+// var i = 1;
+// let duplic = (element) =>
+// {
+    //    i++;
+    //    clone = element.cloneNode(true);
+    //    clone.name = element + '' + i;
+    // }
+    
+    // duplic(clonage)
