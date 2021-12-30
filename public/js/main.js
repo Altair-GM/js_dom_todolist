1c9755bd0deebe9fd29e2b2cfd213719ba970375
Altair-GM <ghannou.0702@gmail.com>
2021-12-30 14:42:22 +0100

    End+Keypress-Enter-

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,7 @@
-import { mutiClonage, monsterGenerator } from "./modules.js/fonctions.js";
+import {
+    mutiClonage,
+    monsterGenerator
+} from "./modules.js/fonctions.js";
 /* ----------------------------- Classes bbutton ---------------------------- */
 let allCmnd = document.querySelector(".allCmnd").children;
 let todo = document.querySelector(".todo").children;
@@ -21,39 +24,36 @@ console.log(delAll);
 /* --------------------------- Commandes generales -------------------------- */
 
 ended.addEventListener("click", () => {
-  let allitems = document.querySelectorAll(".liste>div");
-  allitems.forEach((e) => {
-    e.style.display = "";
-  });
-  allitems.forEach((e) => {
-    if (e.children[0].style.backgroundColor !== "green") {
-      e.style.display = "none";
-    }
-  });
-});
-toDo.addEventListener("click", () => {
-  let allitems = document.querySelectorAll(".liste>div");
-  allitems.forEach((e) => {
-    e.style.display = "";
-  });
-  allitems.forEach((e) => {
-    if (e.children[0].style.backgroundColor === "green") {
-      e.style.display = "none";
-    }
-  });
-});
-all.addEventListener("click", () => {
-  let allitems = document.querySelectorAll(".liste>div");
+    let allitems = document.querySelectorAll(".liste>div");
     allitems.forEach((e) => {
         e.style.display = "";
-      });
+    });
+    allitems.forEach((e) => {
+        if (e.children[0].style.backgroundColor !== "green") {
+            e.style.display = "none";
+        }
+    });
+});
+toDo.addEventListener("click", () => {
+    let allitems = document.querySelectorAll(".liste>div");
+    allitems.forEach((e) => {
+        e.style.display = "";
+    });
+    allitems.forEach((e) => {
+        if (e.children[0].style.backgroundColor === "green") {
+            e.style.display = "none";
+        }
+    });
+});
+all.addEventListener("click", () => {
+    let allitems = document.querySelectorAll(".liste>div");
+    allitems.forEach((e) => {
+        e.style.display = "";
+    });
 });
 /* ----------------------------- commandes input ---------------------------- */
 
-add.addEventListener("click", (e) => {
-    /* ------------------------------- iv phantom ------------------------------- */
-    
-    
+let createDiv = () => {
     let input2 = document.createElement("input");
     let btn1 = document.createElement("button");
     btn1.innerHTML = '<i class="far fa-check-circle"></i>';
@@ -62,43 +62,64 @@ add.addEventListener("click", (e) => {
     let btn3 = document.createElement("button");
     btn3.innerHTML = '<i class="fas fa-backspace"></i>';
     let mdl = [input2, btn1, btn2, btn3];
+
+
+    console.log();
+    if (input.value == "") {
+        alert("Villez rentrere une tache");
+    }
+
+    btn1.addEventListener("click", () => {
+        if (input2.style.backgroundColor == "green") {
+            input2.style.backgroundColor = "red";
+            // input.style.backgroundColor = "black"
+        } else {
+            input2.style.backgroundColor = "green";
+            // input.style.backgroundColor = "white"
+        }
+    });
+    btn2.addEventListener("click", () => {
+        if (input2.disabled == true) {
+            input2.disabled = false;
+        } else {
+            input2.disabled = true;
+        }
+    });
+    btn3.addEventListener("click", (e) => {
+        let tache = e.target;
+        let tache2 = tache.parentNode;
+        let tache3 = tache2.parentNode;
+        tache3.remove();
+    });
+
+    input2.value = input.value;
+    input2.disabled = true;
+    monsterGenerator(mdl);
+
+    // mutiClonage(liste)
+    input.value = "";
+}
+add.addEventListener("click", (e) => {
+    createDiv()
     
-    // console.log(e);
-    // if (input.value == "") {
-    //   alert("Villez rentrere une tache");
-    // }
+    });
+    /* ------------------------------- iv phantom ------------------------------- */
+/* ----------------------------- Fonction Enter ----------------------------- */
 
-  btn1.addEventListener("click", () => {
-    if (input2.style.backgroundColor == "green") {
-      input2.style.backgroundColor = "red";
-      // input.style.backgroundColor = "black"
-    } else {
-      input2.style.backgroundColor = "green";
-      // input.style.backgroundColor = "white"
-    }
-  });
-  btn2.addEventListener("click", () => {
-    if (input2.disabled == true) {
-      input2.disabled = false;
-    } else {
-      input2.disabled = true;
-    }
-  });
-  btn3.addEventListener("click", (e) => {
-    let tache = e.target;
-    let tache2 = tache.parentNode;
-    let tache3 = tache2.parentNode;
-    tache3.remove();
-  });
+console.log(add);
+input.addEventListener("keypress", (e) => {
+    console.log(e.key);
+            if (e.key === 'Enter') {
+                createDiv()
 
-  input2.value = input.value;
-  input2.disabled = true;
-  monsterGenerator(mdl);
 
-  // mutiClonage(liste)
-  input.value = "";
+            }
 });
+
+
+
+/* ------------------------------ // Delet All ------------------------------ */
 console.log(liste);
 delAll.addEventListener("click", () => {
-  liste.textContent = "";
+    liste.textContent = "";
 });
